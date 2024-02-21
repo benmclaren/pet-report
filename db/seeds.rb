8f75c1b7684d6507651b2d11a4f4723dffec2e9e
Ben McLaren <benmclaren@live.co.uk>
2024-02-21 14:30:36 +0000

    added code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Pet.create(name: 'Leonardo', address: "Sewer lair", species: "turtle")
+Pet.create(name: 'Raphael', address: "Sewer lair", species: "turtle")
+Pet.create(name: 'Donatello', address: "Sewer lair", species: "turtle")
+Pet.create(name: 'Michaelangelo', address: "Sewer lair", species: "turtle")
+Pet.create(name: 'Cleo', address: "Basecamp, species: "dog")
+Pet.create(name: 'Riley', address: "HQ", species: "dog")
+Pet.create(name: 'Garfield', address: "Bromley", species: "cat")
+
+
+
+
+
+
+
+
